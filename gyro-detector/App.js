ed4cf601c6b3dad682bd5c3b0dd90ad6489c74c1
Simas2006 <sjuknelis@gmail.com>
2018-03-24 22:04:21 -0400

    Add code for Z axis

--- gyro-detector/App.js
+++ gyro-detector/App.js
@@ -23,13 +23,17 @@ export default class AccelerometerSensor extends React.Component {
     }
     var position = this.state.accelerometerData;
     if ( this.state.recentCalls == 6 ) {
-      if ( position.x < 0.2 && position.x > -0.2 ) this.state.recentCalls = 0;
+      if ( (position.x < 0.2 && position.x > -0.2) || (position.z < 0.2 && position.z > -0.2) ) this.state.recentCalls = 0;
       return null
     }
     if ( this.state.recentCalls <= 5 && this.state.recentCalls > 0 ) this.state.recentCalls--;
     if ( this.state.recentCalls > 0 ) return null;
     if ( position.x >= 1 || position.x <= -1 ) {
       directions.x = Math.sign(position.x);
+      this.state.recentCalls = 6;
+    }
+    if ( position.z >= 1 || position.z <= -1 ) {
+      directions.z = Math.sign(position.z);
       this.state.recentCalls = 6;
     }
     console.log(directions);
