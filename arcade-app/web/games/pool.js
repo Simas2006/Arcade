be7735aec0a858e807f31031bd066237c67a8746
Simas2006 <sjuknelis@gmail.com>
2018-03-30 13:57:14 -0400

    Handle pockets + add shown flag

--- arcade-app/web/games/pool.js
+++ arcade-app/web/games/pool.js
@@ -3,10 +3,12 @@ class Pool {
     this.gameState = {
       balls: "0".repeat(16).split("").map(function(item,index) {
         return {
-          x: 70 * (index % 8) + 70,
-          y: 70 * Math.floor(index / 8) + 70,
+          x: 70,
+          y: 70 * (index % 15) + 70,
           a: Math.floor(Math.random() * 360),
-          v: 0
+          v: 0,
+          shown: true,
+          pocketActive: false
         }
       }),
       cue: {
@@ -17,6 +19,12 @@ class Pool {
         shooting: false,
         setVelocity: 0,
         rotationDirection: 0
+      },
+      players: {
+        solid: 0,
+        stripe: 0,
+        winner: 0,
+        active: 1
       }
     }
   }
@@ -38,19 +46,56 @@ class Pool {
       balls[i].x += balls[i].v * Math.cos(Math.PI * balls[i].a / 180);
       balls[i].y += balls[i].v * Math.sin(Math.PI * balls[i].a / 180);
       balls[i].v *= 0.975;
+      var pocketRadius = 65;
       if ( balls[i].v <= 1e-2 ) balls[i].v = 0;
-      if ( balls[i].x <= 50 ||
-          balls[i].x >= canvas.width - 80 ||
-          balls[i].y <= 50 ||
-          balls[i].y >= canvas.height - 80 ) balls[i].a += 90;
-      for ( var j = i + 1; j < balls.length; j++ ) {
-        var xDist = balls[i].x - balls[j].x;
-        var yDist = balls[i].y - balls[j].y;
-        var distance = Math.sqrt(xDist * xDist + yDist * yDist);
-        if ( distance <= 60 ) {
-          balls[i].a = balls[j].a;
-          balls[i].v = balls[j].v * 0.8;
-          balls[j].a += 90;
+      if (
+        balls[i].x <= 50 ||
+        balls[i].x >= canvas.width - 50 ||
+        balls[i].y <= 50 ||
+        balls[i].y >= canvas.height - 50
+      ) balls[i].a += 90;
+      if (
+        balls[i].x <= pocketRadius && balls[i].y <= pocketRadius ||
+        balls[i].x <= pocketRadius && balls[i].y >= canvas.height - pocketRadius ||
+        balls[i].x >= canvas.width - pocketRadius && balls[i].y <= pocketRadius ||
+        balls[i].x >= canvas.width - pocketRadius && balls[i].y >= canvas.height - pocketRadius ||
+        balls[i].y <= pocketRadius && balls[i].x >= canvas.width / 2 - pocketRadius && balls[i].x <= canvas.width / 2 + pocketRadius ||
+        balls[i].y >= canvas.height - pocketRadius && balls[i].x >= canvas.width / 2 - pocketRadius && balls[i].x <= canvas.width / 2 + pocketRadius
+      ) {
+        if ( ! balls[i].pocketActive ) {
+          balls[i].shown = false;
+          balls[i].pocketActive = true;
+          if ( i == 15 ) {
+            setTimeout(function() {
+              balls[15].x = Math.floor(Math.random() * (canvas.width - 50));
+              balls[15].y = Math.floor(Math.random() * (canvas.height - 50));
+              balls[15].v = 0;
+              balls[15].shown = true;
+            },1000);
+          } else if ( i == 8 ) {
+            currentlyLoaded.gameState.players.winner = currentlyLoaded.gameState.players.active == 1 ? 2 : 1;
+          } else {
+            currentlyLoaded.gameState.players.active = currentlyLoaded.gameState.players.active == 1 ? 2 : 1;
+            if ( i <= 7 ) currentlyLoaded.gameState.players.solid++;
+            else currentlyLoaded.gameState.players.stripe++;
+          }
+          var x = i + 1 - 1; // remove pointer
+          setTimeout(function() {
+            balls[x].pocketActive = false;
+          },2000)
+        }
+      }
+      if ( balls[i].shown ) {
+        for ( var j = i + 1; j < balls.length; j++ ) {
+          if ( ! balls[j].shown ) continue;
+          var xDist = balls[i].x - balls[j].x;
+          var yDist = balls[i].y - balls[j].y;
+          var distance = Math.sqrt(xDist * xDist + yDist * yDist);
+          if ( distance <= 60 ) {
+            balls[i].a = balls[j].a;
+            balls[i].v = balls[j].v * 0.8;
+            balls[j].a += 90;
+          }
         }
       }
     }
@@ -78,6 +123,7 @@ class Pool {
             currentlyLoaded.gameState.balls[15].a = 180 + currentlyLoaded.gameState.cue.a;
             currentlyLoaded.gameState.balls[15].v = currentlyLoaded.gameState.cue.setVelocity * 0.125;
             currentlyLoaded.gameState.cue.setVelocity = 0;
+            currentlyLoaded.gameState.players.active = currentlyLoaded.gameState.players.active == 1 ? 2 : 1;
           }
         }
       }
@@ -99,6 +145,7 @@ class Pool {
     }
     var colors = ["yellow","blue","red","purple","orange","#35ec63","brown","black"];
     for ( var i = 0; i < balls.length; i++ ) {
+      if ( ! balls[i].shown ) continue;
       ctx.fillStyle = colors[i > 7 ? i - 8 : i];
       if ( i == 15 ) ctx.fillStyle = "white";
       ctx.beginPath();
@@ -135,12 +182,12 @@ class Pool {
   	ctx.lineTo(canvas.width - 20,canvas.height - 20);
   	ctx.fill();
     ctx.beginPath();
-  	ctx.arc(20,canvas.height / 2,pocketRadius,0.5 * Math.PI,1.5 * Math.PI,false);
-  	ctx.lineTo(20,canvas.height / 2);
+  	ctx.arc(canvas.width / 2,20,pocketRadius,1 * Math.PI,2 * Math.PI,false);
+  	ctx.lineTo(canvas.width / 2,20);
   	ctx.fill();
     ctx.beginPath();
-  	ctx.arc(canvas.width - 20,canvas.height / 2,pocketRadius,1.5 * Math.PI,0.5 * Math.PI,false);
-  	ctx.lineTo(canvas.width - 20,canvas.height / 2);
-  	ctx.fill();
+    ctx.arc(canvas.width / 2,canvas.height - 20,pocketRadius,0 * Math.PI,1 * Math.PI,false);
+    ctx.lineTo(canvas.width / 2,canvas.height - 20);
+    ctx.fill();
   }
 }
