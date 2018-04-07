8eb04ffa2f6e0c3d949b95114a178311d3415c8a
Simas2006 <sjuknelis@gmail.com>
2018-04-07 18:37:35 -0400

    Add code to move ball + bounce

--- arcade-app/web/games/pingpong.js
+++ arcade-app/web/games/pingpong.js
@@ -3,19 +3,25 @@ class PingPong {
     this.gameState = {
       paddles: [
         {
-          x: 200,
-          v: 100
+          x: 400,
+          v: 100,
+          saveV: 0,
+          shooting: 0
         },
         {
-          x: 200,
-          v: 100
+          x: 400,
+          v: 100,
+          saveV: 0,
+          shooting: 0
         }
       ],
       ball: {
-        x: 100,
-        y: 100,
+        x: 200,
+        y: 0,
         g: 100,
-        a: 0
+        a: 90,
+        v: 100,
+        goingUp: false
       }
     }
   }
@@ -29,20 +35,31 @@ class PingPong {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#824210";
+    ctx.fillStyle = "green";
     ctx.fillRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle = "white";
     ctx.fillRect(0,0,canvas.width,canvas.height * 0.05);
     ctx.fillRect(0,canvas.height * 0.95,canvas.width,canvas.height * 0.05);
+    ctx.fillStyle = "gray";
+    ctx.fillRect(0,canvas.height / 2 - 10,canvas.width,20);
     var paddles = currentlyLoaded.gameState.paddles;
     ctx.fillStyle = "red";
     ctx.fillRect(paddles[0].x - 100,canvas.height * 0.07 * ((100 - paddles[0].v) / 100),200,canvas.height * 0.05);
     ctx.fillStyle = "blue";
-    ctx.fillRect(paddles[1].x - 100,canvas.height * (1 - 0.07 * ((100 - paddles[1].v) / 100) - 0.05),200,canvas.height * 0.05);
+    ctx.fillRect(paddles[1].x - 100,canvas.height * (1 - 0.15 * ((100 - paddles[1].v) / 100) - 0.05),200,canvas.height * 0.05);
     var ball = currentlyLoaded.gameState.ball;
     ctx.fillStyle = "white";
     ctx.beginPath();
     ctx.arc(ball.x,ball.y,5 + 20 * (ball.g / 100),0,2 * Math.PI);
     ctx.fill();
+    ball.x += (ball.v / 50) * Math.cos(Math.PI * ball.a / 180);
+    ball.y += (ball.v / 50) * Math.sin(Math.PI * ball.a / 180);
+    ball.v *= 0.998;
+    ball.g += ball.goingUp ? 1 : -1;
+    if ( ball.g <= 0 ) {
+      if ( ! ball.stopped ) ball.goingUp = true;
+      else ball.g = 1;
+    }
+    if ( ball.g >= ball.v * 1.5 ) ball.goingUp = false;
   }
 }
