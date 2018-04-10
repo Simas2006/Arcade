85491768d949fd8afeaeda48ca328b7d7532f10c
Simas2006 <sjuknelis@gmail.com>
2018-04-09 21:43:47 -0400

    Add more complicated interactivity to server

--- arcade-app/server.js
+++ arcade-app/server.js
@@ -3,15 +3,50 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var PORT = process.argv[2] || 8000;
-var directions = {a:0,x:0,z:0};
+var codes = [];
 var activeSocket;
 
 app.use("/web",express.static(__dirname + "/web"));
 
-app.get("/send",function(request,response) {
+app.get("/internal/connect",function(request,response) {
+  if ( codes.length < 2 ) {
+    var random = Math.floor(Math.random() * 1e8);
+    codes.push(random);
+    response.writeHead(200);
+    response.write(random.toString());
+    response.end();
+  } else {
+    activeSocket.emit("instruction",`{"type":"connection"}`);
+    response.writeHead(200);
+    response.write("err_not_available");
+    response.end();
+  }
+});
+
+app.get("/internal/disconnect",function(request,response) {
   var qs = decodeURIComponent(request.url.split("?").slice(1).join("?"));
-  directions = JSON.parse(qs);
-  activeSocket.emit("directions",qs);
+  codes = codes.filter(item => item != qs);
+  activeSocket.emit("instruction",`{"type":"disconnection"}`);
+  response.writeHead(200);
+  response.write("ok");
+  response.end();
+});
+
+app.get("/internal/move",function(request,response) {
+  var qs = decodeURIComponent(request.url.split("?").slice(1).join("?"));
+  var data = JSON.parse(qs);
+  if ( codes.indexOf(data.code) <= -1 ) {
+    response.writeHead(200);
+    response.write("err_invalid_code");
+    response.end();
+    return;
+  }
+  var obj = {
+    type: "direction",
+    controller: codes.indexOf(data.code),
+    data: data
+  }
+  activeSocket.emit("instruction",JSON.stringify(obj));
   response.writeHead(200);
   response.write("ok");
   response.end();
