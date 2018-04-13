e4c6d27b91b1c658451f48bbcfe687b00b768949
Simas2006 <sjuknelis@gmail.com>
2018-04-13 14:28:37 -0400

    Add code to web client for multi-controller input

--- arcade-app/web/script.js
+++ arcade-app/web/script.js
@@ -5,18 +5,27 @@ var games = {
   pacman: new Pacman(),
   pingpong: new PingPong()
 }
-var currentlyLoaded = games.pingpong;
+var currentlyLoaded = null;
+var activeControllers = 0;
 
-socket.on("directions",function(data) {
-  data = JSON.parse(data);
-  if ( data.x > 0 ) currentlyLoaded.directionalAPI.x.positive();
-  if ( data.x < 0 ) currentlyLoaded.directionalAPI.x.negative();
-  if ( data.z > 0 ) currentlyLoaded.directionalAPI.z.positive();
-  if ( data.z < 0 ) currentlyLoaded.directionalAPI.z.negative();
-  if ( data.a > 0 ) {
-    // run home code
+socket.on("instruction",function(message) {
+  message = JSON.parse(message);
+  if ( message.type == "connection" ) activeControllers++;
+  if ( message.type == "disconnection" ) activeControllers--;
+  if ( message.type == "direction" ) {
+    if ( currentlyLoaded.metadata.controllerType == 0 && message.controller != 0 ) return;
+    if ( currentlyLoaded.metadata.controllerType == 1 && message.controller != currentlyLoaded.metadata.turn ) return;
+    var api = currentlyLoaded.directionalAPI;
+    if ( currentlyLoaded.metadata.controllerType == 2 ) api = currentlyLoaded.directionalAPI[message.controller];
+    if ( message.data.x > 0 ) api.x.positive();
+    if ( message.data.x < 0 ) api.x.negative();
+    if ( message.data.z > 0 ) api.z.negative();
+    if ( message.data.z < 0 ) api.z.positive();
+    if ( message.data.a > 0 ) api.a();
+    if ( message.data.a < 0 ) {
+      // run home code
+    }
   }
-  if ( data.a < 0 ) currentlyLoaded.directionalAPI.a();
 });
 
-window.onload = currentlyLoaded.init;
+if ( currentlyLoaded ) window.onload = currentlyLoaded.init;
